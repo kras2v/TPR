6864a490904b5e896c8a7d983382c58a237743bc
Valeriia <cvetlya4ek@gmail.com>
2024-04-17 17:42:42 +0200

    Tests added

---
+++ LB2/LB2Tests/Orders/BookOrderTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LB2.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB2.Orders.Tests
+{
+    [TestClass()]
+    public class BookOrderTests
+    {
+        [TestMethod()]
+        public void ProcessTest()
+        {
+            // Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+            var bookOrder = new BookOrder();
+
+            // Act
+            bookOrder.Process();
+            string result = writer.ToString();
+
+            // Assert
+            Assert.AreEqual("Processing book order...", result.Trim());
+        }
+    }
+}
